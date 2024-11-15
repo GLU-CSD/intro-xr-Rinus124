4350fcfec89b4552accfa7afc0f103486d31a5e5
Rinus124 <marijnvanveggel3@gmail.com>
2024-11-15 12:49:13 +0100

    Final XR intro week 1

--- Assets/Models/My work/Prefab/Health.cs
+++ Assets/Models/My work/Prefab/Health.cs
@@ -1,24 +1,31 @@
+// gebruikt de assets van Unity zelf en de UI's van Unity
 using UnityEngine;
 using UnityEngine.UI;
 
+//Start van het gehele script met de naam van het script zelf
 public class Health : MonoBehaviour
 {
+    /// <summary>
+    ///  variabelen
+    /// </summary>
     public float maxHealth = 100f;
     public float currentHealth;
     public Image Healthbarfill;
 
-
+    // functie om de Healthbar te updaten wanneer er op een button wordt geklikt
     void UpdateHealthbar()
     {
         Healthbarfill.fillAmount = currentHealth / maxHealth;
     }
 
+    //Het begin van het script (dit gebeurd als aller eerst)
     void Start()
     {
         currentHealth = maxHealth;
         UpdateHealthbar();
     }
 
+    //wanneer er op de damage button wordt geklikt (public functie dus die kan worden terug gevonden)
     public void TakeDamage(float amount)
     {
         currentHealth -= amount;
@@ -26,6 +33,7 @@ public class Health : MonoBehaviour
         UpdateHealthbar();
     }
 
+    //wanneer er op de Health button wordt geklikt (public functie dus die kan worden terug gevonden)
     public void RestoreHealth(float amount)
     {
         currentHealth += amount;
--- Assets/Models/My work/Prefab/HealthUI.cs
+++ Assets/Models/My work/Prefab/HealthUI.cs
@@ -1,8 +1,11 @@
+// gebruikt de assets van Unity zelf en de UI's van Unity
 using UnityEngine;
 using UnityEngine.UI;
 
+//Start van het gehele script met de naam van het script zelf
 public class HealthUI : MonoBehaviour
 {
+    //variabelen (public zijn in andere scripts zichtbaar en private niet)
     public string prefabTag = "magician_rio_unity";
     private Health healthScript;
     void Start()
@@ -10,6 +13,7 @@ public class HealthUI : MonoBehaviour
 
     }
 
+    // functie die zich steeds herhaald
     void Update()
     {
         if (healthScript == null)
@@ -22,6 +26,7 @@ public class HealthUI : MonoBehaviour
             }
         }
     }
+    //als damage button wordt geklikt
     public void DamageButton()
     {
         if (healthScript != null)
@@ -29,7 +34,7 @@ public class HealthUI : MonoBehaviour
             healthScript.TakeDamage(10);
         }
     }
-
+    //als Heal button wordt geklikt
     public void HealButton()
     {
         if (healthScript != null)
